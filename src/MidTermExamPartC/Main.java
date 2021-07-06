cf86c76b76056b1950c22fa19802ac2eeced4a32
pagtolon <84794766+pagtolon@users.noreply.github.com>
2021-07-06 12:19:42 -0400

    Third Commit...Imported Scanner

--- src/MidTermExamPartC/Main.java
+++ src/MidTermExamPartC/Main.java
@@ -5,6 +5,7 @@
  */
 package MidTermExamPartC;
 
+import java.util.Scanner;
 /**
  *
  * @author Lanzp
@@ -16,6 +17,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner input = new Scanner(System.in);
     }
     
 }
